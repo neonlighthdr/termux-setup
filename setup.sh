2c80a56d8caadea8ece20368a9d1a7c0bc9c090c
neonlighthdr <neonlighthdr@gmail.com>
2025-11-19 12:44:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 #idk
 termux-setup-storage
+sleep 2
 curl -L https://github.com/neonlighthdr/termux-setup/raw/refs/heads/main/change-repo/termux-fastest-repo | bash
 pkg update
+sleep 2
 
 # update installed package but keep configuration
 pkg upgrade -y -o Dpkg::Options::="--force-confold"
-sleep 2
+sleep 1
 
 # idk
 pkg install -y x11-repo tur-repo
 sleep 2
 # idk
-pkg install -y curl wget git nano proot-distro termux-x11 pulseaudio
+pkg install -y curl wget git nano 
+pkg install -y proot-distro pulseaudio
+sleep 1
 # idk 
-pkg install -y termux-x11-nightly
+pkg install termux-x11 && pkg install -y termux-x11-nightly
 
 clear
 
